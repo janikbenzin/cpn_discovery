275bbc0c2bc6c9768290de7dc49ce7cceef16f39
Janik 'Janik' Benzin <janikbenzin@gmail.com>
2024-01-29 16:11:33 +0100

    Published version

---
+++ corradinietal/README.md
@@ -0,0 +1,20 @@
+# README #
+
+This is repository contains the jar build of the Colliery tool, and the script files for using the Pm4Py libraries.
+
+##Requirements
+
+* Java Runtime Environment 
+* Python v. 3.x
+* [Pm4Py](https://pm4py.fit.fraunhofer.de/install)
+
+## Usage
+
+Make sure to have downloaded in the same directory both the Colliery jar file and the scripts folder. Then execute the jar file with double click or from terminal.
+ 
+Example:
+
+```bash
+java -jar Colliery.jar  
+```
+ 
---
+++ corradinietal/run_original.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+###
+## ProM specific
+###
+PROGRAM=ProM
+CP=./corradinietal/Colliery-1.0.jar
+
+LIBDIR=./corradinietal/lib
+MAIN=it.unicam.pros.colliery.Colliery
+
+####
+## Environment options
+###
+JAVA=/usr/lib/jvm/java-21-openjdk-21.0.1.0.12-4.rolling.fc39.x86_64/bin/java
+
+###
+## Main program
+###
+
+add() {
+  CP=${CP}:$1
+}
+
+
+for lib in $LIBDIR/*.jar
+do
+  add $lib
+done
+
+echo $JAVA -Xmx4G -classpath ${CP} -Djava.library.path=${LIBDIR} ${MAIN} $@
+$JAVA -Xmx4G -classpath ${CP} -Djava.library.path=${LIBDIR} ${MAIN} $@
+
